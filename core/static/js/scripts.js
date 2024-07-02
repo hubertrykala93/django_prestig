30c904ea0a4daed91920f3b20af19d075e1ad106
Szymon Lewandowski <szymon.levy123@gmail.com>
2024-07-02 14:22:54 +0200

    logos carousel & bottom gallery done

--- core/static/js/scripts.js
+++ core/static/js/scripts.js
@@ -100,3 +100,55 @@ navigation: {
     prevEl: '.js-arrow-left',
   },
 });
+
+const logos = new Swiper('.js-logos-carousel', {
+    loop: true,
+    draggable: true,
+    grabCursor: true,
+    effect: 'slide',
+    autoplay: {
+        delay: 500,
+    },
+    speed: 800,
+    slidesPerView: 2,
+    spaceBetween: 50,
+    breakpoints: {
+        480: {
+            slidesPerView: 3,
+        },
+        768: {
+            slidesPerView: 4,
+        },
+        1200: {
+            slidesPerView: 5,
+        }
+    }
+});
+
+const bottomGallery = new Swiper('.js-bottom-gallery-carousel', {
+    loop: true,
+    draggable: true,
+    grabCursor: true,
+    effect: 'slide',
+    autoplay: {
+        delay: 1000,
+    },
+    speed: 800,
+    breakpoints: {
+        480: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        992: {
+            slidesPerView: 4,
+        },
+        1200: {
+            slidesPerView: 5,
+        },
+        1600: {
+            slidesPerView: 6,
+        }
+    }
+});
