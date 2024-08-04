2240e7ae39ea764056801c740bb477f22a512d76
hubertrykala93 <hubert.rykala@gmail.com>
2024-08-04 14:28:50 +0200

    creating routes for article categories and tags

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from . import views as blog_views
 
 urlpatterns = [
     path(route="blog", view=blog_views.blog, name="blog"),
-    path(route="blog/<slug:category_slug>/<slug:article_slug>", view=blog_views.article_details, name="article-details"),
+    path(route="blog/category/<slug:category_slug>", view=blog_views.articles_by_category, name="articles-by-category"),
+    path(route="blog/tag/<slug:tag_slug>", view=blog_views.articles_by_tag, name="articles-by-tag"),
+    path(route="blog/<slug:category_slug>/<slug:article_slug>", view=blog_views.article_details,
+         name="article-details"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -16,6 +16,34 @@ def blog(request):
     )
 
 
+def articles_by_category(request, category_slug):
+    category = ArticleCategory.objects.get(slug=category_slug)
+
+    return render(
+        request=request,
+        template_name="blog/articles-by-category.html",
+        context={
+            "title": category.name,
+            "category": category,
+            "articles": Article.objects.filter(article_category=category)
+        }
+    )
+
+
+def articles_by_tag(request, tag_slug):
+    tag = ArticleTag.objects.get(slug=tag_slug)
+
+    return render(
+        request=request,
+        template_name="blog/articles-by-tag.html",
+        context={
+            "title": tag.name,
+            "tag": tag,
+            "articles": Article.objects.filter(article_tag=tag),
+        }
+    )
+
+
 def article_details(request, category_slug, article_slug):
     category = ArticleCategory.objects.get(slug=category_slug)
     article = Article.objects.get(slug=article_slug, article_category=category)
