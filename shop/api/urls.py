9fdc1bfb0a214658bc25226a556c8820a3431be8
hubertrykala93 <hubert.rykala@gmail.com>
2024-08-12 02:03:08 +0200

    update delivery details serializer

--- shop/api/urls.py
+++ shop/api/urls.py
@@ -1,10 +1,2 @@
 from django.urls import path
 from . import views
-
-urlpatterns = [
-    path(
-        route="api/v1/deliveries/delivery-details/<int:pk>",
-        view=views.DeliveryDetailsDetailsAPIView.as_view(),
-        name="api-v1-deliveries-delivery-details",
-    ),
-]
--- shop/api/views.py
+++ shop/api/views.py
@@ -3,37 +3,3 @@ from rest_framework import status
 from rest_framework.generics import RetrieveAPIView
 from shop.models import DeliveryDetails
 from .serializers import DeliveryDetailsSerializer
-
-
-class DeliveryDetailsDetailsAPIView(RetrieveAPIView):
-    def get_queryset(self):
-        return DeliveryDetails.objects.all()
-
-    def get_serializer_class(self):
-        return DeliveryDetailsSerializer
-
-    def get(self, request, *args, **kwargs):
-        try:
-            delivery_details = self.get_object()
-            serializer = self.get_serializer(delivery_details)
-
-            return Response(
-                data=serializer.data,
-                status=status.HTTP_200_OK,
-            )
-
-        except DeliveryDetails.DoesNotExist:
-            return Response(
-                data={
-                    "error": f"Delivery Details with ID {self.kwargs.get('pk')} does not exist.",
-                },
-                status=status.HTTP_404_NOT_FOUND,
-            )
-
-        except Exception as e:
-            return Response(
-                data={
-                    "error": str(e),
-                },
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            )
