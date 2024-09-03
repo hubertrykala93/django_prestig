610973257f3ecef798d6d022fedc32ce7917a13a
hubertrykala93 <hubert.rykala@gmail.com>
2024-09-03 10:23:54 +0200

    working on render categories and subcategories

--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
-from .models import ProductSubCategory, ProductCategory, Size, Color, Brand
+from .models import ProductSubCategory, ProductCategory, Size, Color, Brand, Product
 
 
 def shop(request):
-    categories = ProductCategory.objects.all()
+    queryset = Product.objects.filter(is_active=True).order_by("-created_at")
+
+    categories = ProductCategory.objects.prefetch_related("category").all()
+
     for category in categories:
-        print(category.name)
-        print(category.category.all())
+        print(f"Category -> {category.name}")
+
+        subcategories = category.category.all()
+
+        for subcategory in subcategories:
+            print(f"Subcategory -> {subcategory.name}")
+
+    if request.GET:
+        if "sort" in request.GET:
+            if request.GET["sort"] == "default":
+                queryset = Product.objects.filter(is_active=True).order_by("-created_at")
+
+            if request.GET["sort"] == "alphabetically":
+                queryset = Product.objects.filter(is_active=True).order_by("-name")
+
+            if request.GET["sort"] == "price":
+                queryset = Product.objects.filter(is_active=True).order_by("price")
 
     return render(
         request=request,
@@ -14,6 +32,7 @@ def shop(request):
         context={
             "title": "Shop",
             "img": "/media/page-title/shop.jpg",
+            "queryset": queryset,
             "sizes": Size.objects.all(),
             "colors": Color.objects.all().order_by("-name"),
             "brands": Brand.objects.all().order_by("name"),
